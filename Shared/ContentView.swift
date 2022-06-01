406d344307438fa313743b456c74f953d7d5e31f
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-01 18:59:40 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 08/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            Image("background")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+
+            VStack {
+                
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Image("card3")
+                    
+                    Spacer()
+                    
+                    Image("card4")
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    print("Card dealt!")
+                    
+                }, label: {
+                    
+                    Image("dealbutton")
+                    
+                })
+                
+                Spacer()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    VStack {
+                        
+                        Text("Player")
+                            .foregroundColor(.white)
+                            .padding(.bottom)
+                            .font(.title2)
+                        
+                        Text("0")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        
+                        Text("CPU")
+                            .foregroundColor(.white)
+                            .padding(.bottom)
+                            .font(.title2)
+                        
+                        Text("0")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
